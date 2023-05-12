00541fbc810c0bdf620f1b9f21e77bd6fe787bb5
alessandrini11 <58996105+alessandrini11@users.noreply.github.com>
2023-05-12 22:48:18 +0100

    redirect user to login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,30 +17,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(Request $request, 
-        VisitorService $visitorService, 
-        FrontendRepository $frontendRepository, 
-        BackendRepository $backendRepository,
-        VisitorRepository $visitorRepository
-    ): Response
+    public function index(): Response
     {
-        $ip = $request->getClientIp();
-        $visitorService->getVisitorByIp($ip);
-        return $this->render('home/index.html.twig', [
-            'frontend' => $frontendRepository->findAll(),
-            'backend' => $backendRepository->findAll(),
-            'visitors' => $visitorRepository->findAll()
-        ]);
-    }
-
-    #[Route('/projects', name: 'app_project', methods: ['GET'])]
-    public function project(ProjectRepository $projectRepository, Request $request, VisitorService $visitorService): Response
-    {
-        $ip = $request->getClientIp();
-        $visitorService->getVisitorByIp($ip);
-        return $this->render('home/project.html.twig', [
-            'projects' => $projectRepository->findAll()
-        ]);
+        return $this->redirectToRoute('admin');
     }
 
     #[Route('/download-cv', name: 'app_cv', methods:['GET'])]
@@ -52,12 +31,4 @@ class HomeController extends AbstractController
         );
         return $this->file($cvPath, 'cv.pdf');
     }
-
-    // #[Route('/update-photo', name: 'app_photo', methods: ['POST'])]
-    // public function updatePhoto(Request $request): Response
-    // {
-    //     // dd($request);
-    //     dd($request->files->get('photo'));
-    //     return $this->redirect('admin');
-    // }
 }
